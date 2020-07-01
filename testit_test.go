31d0352f76e66c7828ce79d503a060d4d3a4c143
enthusiast17 <ulan17nurym@gmail.com>
2020-07-01 22:16:02 +0500

    feat: add log in test

--- testit_test.go
+++ testit_test.go
@@ -11,16 +11,26 @@ func TestItTest(t *testing.T) {
 	a, b := 1, 1
 	if err := equal(a, b); err != nil {
 		t.Errorf(message.Fail, nil, err)
+	} else {
+		t.Logf(message.PassEqual, err, nil)
 	}
+
 	if err := notEqual(a, b); err == nil {
 		t.Errorf(message.Fail, fmt.Errorf(message.FailNotEqual, a, b, a, b), err)
+	} else {
+		t.Logf(message.PassEqual, err, fmt.Errorf(message.FailNotEqual, a, b, a, b))
 	}
 
 	as, bs := "hello", "there"
 	if err := notEqual(as, bs); err != nil {
 		t.Errorf(message.Fail, nil, err)
+	} else {
+		t.Logf(message.PassEqual, err, nil)
 	}
+
 	if err := equal(as, bs); err == nil {
 		t.Errorf(message.Fail, fmt.Errorf(message.FailEqual, as, bs, as, bs), err)
+	} else {
+		t.Logf(message.PassEqual, err, fmt.Errorf(message.FailEqual, as, bs, as, bs))
 	}
 }
