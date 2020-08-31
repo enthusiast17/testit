db192ec288e8732a4ce3ff03feca7018983ce400
enthusiast17 <ulan17nurym@gmail.com>
2020-08-31 16:40:21 +0500

    feat: add 'called from'

--- testit.go
+++ testit.go
@@ -2,37 +2,46 @@ package testit
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 
-	message "github.com/enthusiast17/testit/message"
+	message "./message"
 )
 
 // Equal prints FAIL/PASS (1 interface is expected value, 2 interface is value for compare).
 func Equal(t *testing.T, i1, i2 interface{}) {
-	if err := equal(i1, i2); err != nil {
+	_, file, line, ok := runtime.Caller(1)
+	if !ok {
+		file, line = "nil", -1
+	}
+	if err := equal(i1, i2, file, line); err != nil {
 		t.Error(err)
 	}
-	t.Logf(message.PassEqual, i1, i2)
+	t.Logf(message.PassEqual, i1, i2, file, line)
 }
 
 // NotEqual prints FAIL/PASS (1 interface is expected value, 2 interface is value for compare).
 func NotEqual(t *testing.T, i1, i2 interface{}) {
-	if err := notEqual(i1, i2); err != nil {
+	_, file, line, ok := runtime.Caller(1)
+	if !ok {
+		file, line = "nil", -1
+	}
+	if err := notEqual(i1, i2, file, line); err != nil {
 		t.Error(err)
 	}
-	t.Logf(message.PassNotEqual, i1, i2)
+	t.Logf(message.PassNotEqual, i1, i2, file, line)
 }
 
-func equal(i1, i2 interface{}) error {
+func equal(i1, i2, file, line interface{}) error {
 	if i1 != i2 {
-		return fmt.Errorf(message.FailEqual, i1, i2, i1, i2)
+		return fmt.Errorf(message.FailEqual, i1, i2, i1, i2, file, line)
 	}
 	return nil
 }
 
-func notEqual(i1, i2 interface{}) error {
+func notEqual(i1, i2, file, line interface{}) error {
 	if i1 == i2 {
-		return fmt.Errorf(message.FailNotEqual, i1, i2, i1, i2)
+		return fmt.Errorf(message.FailNotEqual, i1, i2, i1, i2, file, line)
 	}
 	return nil
 }
--- testit_test.go
+++ testit_test.go
@@ -2,35 +2,40 @@ package testit
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 
 	message "./message"
 )
 
 func TestItTest(t *testing.T) {
+	_, file, _, ok := runtime.Caller(1)
+	if !ok {
+		file = "nil"
+	}
 	a, b := 1, 1
-	if err := equal(a, b); err != nil {
-		t.Errorf(message.Fail, nil, err)
+	if err := equal(a, b, file, 18); err != nil {
+		t.Errorf(message.Fail, nil, err, file, 18)
 	} else {
-		t.Logf(message.PassEqual, err, nil)
+		t.Logf(message.PassEqual, err, nil, file, 18)
 	}
 
-	if err := notEqual(a, b); err == nil {
-		t.Errorf(message.Fail, fmt.Errorf(message.FailNotEqual, a, b, a, b), err)
+	if err := notEqual(a, b, file, 24); err == nil {
+		t.Errorf(message.Fail, fmt.Errorf(message.FailNotEqual, a, b, a, b, file, 24), err, file, 24)
 	} else {
-		t.Logf(message.PassEqual, err, fmt.Errorf(message.FailNotEqual, a, b, a, b))
+		t.Logf(message.PassEqual, err, fmt.Errorf(message.FailNotEqual, a, b, a, b, file, 24), file, 24)
 	}
 
 	as, bs := "hello", "there"
-	if err := notEqual(as, bs); err != nil {
-		t.Errorf(message.Fail, nil, err)
+	if err := notEqual(as, bs, file, 31); err != nil {
+		t.Errorf(message.Fail, nil, err, file, 31)
 	} else {
-		t.Logf(message.PassEqual, err, nil)
+		t.Logf(message.PassEqual, err, nil, file, 31)
 	}
 
-	if err := equal(as, bs); err == nil {
-		t.Errorf(message.Fail, fmt.Errorf(message.FailEqual, as, bs, as, bs), err)
+	if err := equal(as, bs, file, 37); err == nil {
+		t.Errorf(message.Fail, fmt.Errorf(message.FailEqual, as, bs, as, bs, file, 37), err, file, 37)
 	} else {
-		t.Logf(message.PassEqual, err, fmt.Errorf(message.FailEqual, as, bs, as, bs))
+		t.Logf(message.PassEqual, err, fmt.Errorf(message.FailEqual, as, bs, as, bs, file, 37), file, 37)
 	}
 }
