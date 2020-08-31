1256483c84b10423978f8057ee35d13bba7f7864
enthusiast17 <ulan17nurym@gmail.com>
2020-08-31 16:39:16 +0500

    refactor: update constant messages

--- message/message.go
+++ message/message.go
@@ -1,16 +1,18 @@
 package message
 
-// Fail constant message when got fail.
-const Fail = "FAIL: Expected [%v], got [%v]"
+const (
+	// Fail constant message when got fail.
+	Fail = "FAIL: Expected [%[1]v], got [%[2]v] | called from [file: %[3]v, line: %[3]v]"
 
-// FailEqual constant message when got fail from equal.
-const FailEqual = "FAIL: Expected [%v == %v], got [%v != %v]"
+	// FailEqual constant message when got fail from equal.
+	FailEqual = "FAIL: Expected [%[1]v == %[2]v], got [%[1]v != %[2]v] | called from [file: %[3]v, line: %[4]v]"
 
-// FailNotEqual constant message when got fail from not equal.
-const FailNotEqual = "FAIL: Expected [%v != %v], got [%v == %v]"
+	// FailNotEqual constant message when got fail from not equal.
+	FailNotEqual = "FAIL: Expected [%[1]v != %[2]v], got [%[1]v == %[2]v] | called from [file: %[3]v, line: %[4]v]"
 
-// PassEqual constant message when got pass from equal.
-const PassEqual = "PASS: [%v] == [%v]"
+	// PassEqual constant message when got pass from equal.
+	PassEqual = "PASS: [%[1]v] == [%[2]v] | called from [file: %[3]v, line: %[4]v]"
 
-// PassNotEqual constant message when got pass from not equal.
-const PassNotEqual = "PASS: [%v] != [%v]"
+	// PassNotEqual constant message when got pass from not equal.
+	PassNotEqual = "PASS: [%[1]v] != [%[2]v] | called from [file: %[3]v, line: %[4]v]"
+)
